9613a2c0db959bf25fe91e429c3dc0e54eaaef74
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-09-24 17:30:20 +0530

    feat: configure PostgreSQL connection and update environment variables

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,25 @@ import "dotenv/config";
 configDotenv()
 import app from "./app.js";
 import { configDotenv } from "dotenv";
+import pool from "./config/db.js";
 
 const PORT = process.env.PORT
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`)
-})
+
+const startServer = async()=>{
+    try {
+        await pool.query("SELECT 1")
+
+        console.log("PostgreSQL connected successfully!");
+        app.listen(PORT, ()=>{
+            console.log(`Server is running on ${PORT}`)
+
+        })
+        
+    } catch (error) {
+        console.error("Failed to connect postgreSQL",error);
+        process.exit(1)
+    }
+}
+
+startServer()
